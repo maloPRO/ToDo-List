b09c51056833a3d3e7b34609ea63fd66a7e3fd22
maloPRO <malovagilbert@gmail.com>
2022-11-08 18:03:54 +0300

    Added Js css files

---
+++ src/projects.js
@@ -0,0 +1,6 @@
+export class Project {
+  constructor (title, category) {
+    this.title = title;
+    this.category = category;
+  }
+}
--- webpack.config.js
+++ webpack.config.js
@@ -6,12 +6,20 @@ module.exports = {
   entry: './src/index.js',
   plugins: [
     new HtmlWebpackPlugin({
-      title: 'Output Management',
+      title: 'To Do',
     }),
   ],
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
     clean:true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
   },
 };
