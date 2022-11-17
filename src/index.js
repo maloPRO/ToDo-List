147abb1dbbab04068211aedcf907f3a5fa4330e7
maloPRO <malovagilbert@gmail.com>
2022-11-17 11:16:19 +0300

    reduced number of files

--- src/index.js
+++ src/index.js
@@ -1,21 +1,42 @@
 import tabSelectionModule from './tabs';
 // eslint-disable-next-line no-unused-vars
 import css from './main.css';
-// eslint-disable-next-line no-unused-vars
-import newTaskModule from './newtask';
-// eslint-disable-next-line no-unused-vars
-import myProjectsModule from './projects';
-// eslint-disable-next-line no-unused-vars
-import allTasksModule from './all-tasks';
-// eslint-disable-next-line no-unused-vars
-import todaysTasksModule from './today';
-// eslint-disable-next-line no-unused-vars
-import upcomingTabModule from './upcoming';
-
 
 tabSelectionModule.tabSelection();
 tabSelectionModule.customProjects();
 tabSelectionModule.defaults();
+
+const newTaskModule = (function () {
+  const taskList = [];
+  class Task {
+    constructor(title, dueDate, description, category) {
+      this.title = title;
+      this.dueDate = dueDate;
+      this.description = description;
+      this.category = category;
+    }
+  }
+
+  const addTask = () => {
+    const title = document.getElementById('title');
+    const category = document.getElementById('category');
+    const dueDate = document.getElementById('dueDate');
+    const description = document.getElementById('description');
+
+    const tasks = new Task(title.value, category.value, dueDate.value, description.value);
+
+    taskList.push(tasks);
+  };
+
+  const openForm = () => { document.getElementById('formContainer').style.display = 'block'; };
+  const closeForm = () => { document.getElementById('formContainer').style.display = 'none'; };
+
+  document.getElementById('closeBtn').addEventListener('click', closeForm);
+  document.getElementById('submitBtn').addEventListener('click', addTask);
+  document.getElementById('formClose').addEventListener('click', closeForm);
+
+  return {openForm};
+}());
 
 const btn = tabSelectionModule.addNewTask;
 
--- src/tabs.js
+++ src/tabs.js
@@ -1,4 +1,7 @@
 import myProjectsModule from './projects';
+import tasksIcon from './images/inboxIcon.png';
+import iconToday from './images/day.png';
+import iconUpcoming from './images/calenderIcon.png';
 
 // eslint-disable-next-line func-names
 const tabSelectionModule = (function () {
@@ -119,7 +122,64 @@ const tabSelectionModule = (function () {
 
 export default tabSelectionModule;
 
+export const allTasksModule = (function () {
+  const allTasksHeader = document.createElement('div');
+  allTasksHeader.setAttribute('id', 'allTasksHeader');
+  allTasksHeader.setAttribute('class', 'sectionHeader');
 
+  const allTasksIcon = new Image()
+  allTasksIcon.src = tasksIcon;
+  allTasksIcon.setAttribute('class', 'tabIcons' );
+  allTasksHeader.appendChild(allTasksIcon);
+
+  const allTasksTitle = document.createElement('div');
+  allTasksTitle.textContent = 'All Tasks';
+  allTasksHeader.appendChild(allTasksTitle);
+
+  tabSelectionModule.allTasksTab.appendChild(allTasksHeader);
+
+  return {}
+    
+})();
+
+export const todaysTasksModule = (function () {
+
+  const todayTasksHeader = document.createElement('div');
+  todayTasksHeader.setAttribute('id', 'todayTasksHeader');
+  todayTasksHeader.setAttribute('class', 'sectionHeader');
+
+  const todayIcon = new Image()
+  todayIcon.src = iconToday;
+  todayIcon.setAttribute('class', 'tabIcons' );
+  todayTasksHeader.appendChild(todayIcon);
+
+  const todayTasksTitle = document.createElement('div');
+  todayTasksTitle.textContent = 'Today\'s Tasks';
+  todayTasksHeader.appendChild(todayTasksTitle);
+
+  tabSelectionModule.todayTab.appendChild(todayTasksHeader)
+
+
+  return { };
+}());
+
+export const upcomingTabModule = (function () {
+  const upcomingTabHeader = document.createElement('div');
+  upcomingTabHeader.setAttribute('id', 'upcomingTabHeader');
+  upcomingTabHeader.setAttribute('class', 'sectionHeader');
+
+  const upcomingIcon = new Image()
+  upcomingIcon.src = iconUpcoming;
+  upcomingIcon.setAttribute('class', 'tabIcons' );
+  upcomingTabHeader.appendChild(upcomingIcon);
+
+  const upcomingTabTitle = document.createElement('div');
+  upcomingTabTitle.textContent = 'Upcoming Tasks';
+  upcomingTabHeader.appendChild(upcomingTabTitle);
+
+  tabSelectionModule.upcomingTab.appendChild(upcomingTabHeader);
+
+})();
 
 
 
