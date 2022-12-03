f975e2c82ca77abe900c36f273666b7c74b9486a
maloPRO <malovagilbert@gmail.com>
2022-12-03 12:13:33 +0300

    created a new directory-modules

---
+++ src/modules/tabs.js
@@ -0,0 +1,104 @@
+import taskicon from '../images/add.png';
+import deleteIcon from '../images/delete.png';
+
+const tabSelectionModule = (function () {
+
+  const defaultTabs = document.querySelectorAll('.tab');
+  const projectTabs = document.querySelectorAll('.projects')
+  const section = document.querySelector('.section');
+  const addTaskDiv = document.createElement('div');
+  let tabTasks = []
+
+  const taskIcon = new Image()
+  taskIcon.src = taskicon;
+
+  addTaskDiv.setAttribute('class', 'addTaskDiv')
+  addTaskDiv.appendChild(taskIcon);
+  
+  defaultTabs.forEach((tab) => {
+    // eslint-disable-next-line prefer-arrow-callback
+
+    tab.addEventListener('click', function () {
+
+      while (section.firstChild) {
+        section.removeChild(section.firstChild);
+      }
+
+      const tabContent = document.createElement('div');
+      const tabNav = document.createElement('div');
+      const tabBody = document.createElement('div');
+      
+      tabBody.setAttribute('class', 'defaultTabBody');
+      tabNav.setAttribute('class', 'tabNav');
+
+      tabNav.textContent = `${this.textContent} Tasks`;
+      tabContent.append(tabNav, tabBody);
+      section.appendChild(tabContent);
+
+    })
+  })
+
+  projectTabs.forEach(tab => {
+    // eslint-disable-next-line prefer-arrow-callback
+    tab.addEventListener('click', function() {
+
+      while (section.firstChild) {
+        section.removeChild(section.firstChild);
+      }
+      const tabContent = document.createElement('div');
+      const tabNav = document.createElement('div');
+      const tabBody = document.createElement('div');
+
+      tabContent.setAttribute('class', `${this.textContent}`)
+      
+      tabNav.setAttribute('class', 'tabNav')
+      tabNav.textContent = `${this.textContent} Tasks`;
+
+      tabBody.classList.add('projectBody');
+      tabBody.classList.add(`${this.textContent}Body`)
+
+      tabBody.appendChild(addTaskDiv)
+      tabContent.append(tabNav, tabBody);
+      section.appendChild(tabContent);
+
+      tabTasks = JSON.parse(localStorage.getItem(`${tabContent.className}`))
+      
+      tabTasks.forEach((tabTask) => {
+        const tasksContainer = document.createElement('div');
+        tasksContainer.classList.add('taskContainer');
+    
+        const checkBox = document.createElement('input');
+        const taskTitle = document.createElement('div');  
+        const taskDate = document.createElement('div');
+        const deleteTask = document.createElement('div');
+        const deleteBtn = new Image();
+    
+        deleteBtn.src = deleteIcon;
+        deleteTask.appendChild(deleteBtn);
+        deleteBtn.classList.add('deleteTaskIcon');
+    
+        checkBox.setAttribute('type', 'checkbox');
+    
+        taskTitle.textContent = tabTask.title;
+        taskDate.textContent = tabTask.dueDate;
+    
+        tasksContainer.append(checkBox, taskTitle, taskDate, deleteTask)
+        tabContent.appendChild(tasksContainer);
+
+        deleteTask.addEventListener('click', function () {
+          this.parentElement.remove();
+          tabTasks.forEach((tab, index) => {
+            tabTasks.splice(index, 1)
+            localStorage.setItem(`${tabContent.className}`, JSON.stringify(tabTasks));
+          })
+        })
+        
+      })
+    })
+  })
+
+
+  return {addTaskDiv, tabTasks }
+})();
+
+export default tabSelectionModule;
---
+++ src/modules/tasks.js
@@ -0,0 +1,84 @@
+import tabSelectionModule from './tabs';
+import deleteIcon from '../images/delete.png';
+
+const tasksModule = (function () {
+  const taskList = [];
+  const taskForm = document.querySelector('#form');
+  const formContainer = document.getElementById('formContainer');
+  const titleInput = document.getElementById('title');
+  const dueDateInput = document.getElementById('dueDate');
+  const cancelBtn = document.getElementById('closeBtn');
+  const addTask = tabSelectionModule.addTaskDiv;
+  const tabTask = tabSelectionModule.tabTasks;
+
+  class Task {
+    constructor (title, dueDate) {
+      this.title = title;
+      this.dueDate = dueDate;
+    }
+  }
+  const createTask = (title, dueDate) => {
+    const task = new Task(titleInput.value, dueDateInput.value);
+    taskList.push(task);
+    tabTask.push(task);
+
+    localStorage.setItem(addTask.parentElement.parentElement.className, JSON.stringify(tabTask))
+    return { title, dueDate }
+
+  }
+  
+  function displayTask ({ title , dueDate }) {
+    const tabContent = addTask.parentElement;
+    
+    const tasksContainer = document.createElement('div');
+    tasksContainer.classList.add('taskContainer');
+
+    const checkBox = document.createElement('input');
+    const taskTitle = document.createElement('div');  
+    const taskDate = document.createElement('div');
+    const deleteTask = document.createElement('div');
+    const deleteBtn = new Image();
+
+    deleteBtn.src = deleteIcon;
+    deleteTask.appendChild(deleteBtn);
+    deleteBtn.classList.add('deleteTaskIcon');
+
+    checkBox.setAttribute('type', 'checkbox');
+
+    taskTitle.textContent = title;
+    taskDate.textContent = dueDate;
+
+    tasksContainer.append(checkBox, taskTitle, taskDate, deleteTask)
+    tabContent.appendChild(tasksContainer);
+
+    deleteTask.addEventListener('click', function () {
+      this.parentElement.remove();
+      
+    })
+  }
+
+  const openForm = () => {
+    formContainer.style.display = 'block';
+  }
+  const closeFrom = () => {
+    formContainer.style.display = 'none';
+  }
+  
+  function handleForm (event) {
+    event.preventDefault();  
+    const task = createTask(titleInput.value, dueDateInput.value)
+    displayTask(task);
+    closeFrom();
+    
+  }
+
+  taskList.forEach(displayTask);
+  taskForm.addEventListener('submit', handleForm)
+  addTask.addEventListener('click', openForm);
+  cancelBtn.addEventListener('click', closeFrom);
+
+  return { displayTask }
+})();
+
+
+export default tasksModule;
